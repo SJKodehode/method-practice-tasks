facdfb68eee39efb5ecc7685715ecaa80ff824fa
Jorgen <sjkodehode2025@gmail.com>
2025-04-29 09:38:38 +0200

    practice tasks

---
+++ app.js
@@ -0,0 +1,38 @@
+  // OPPGAVE 1
+  // Lag en funksjon som kan legge til flere navn inn i et Array, og console.log resultatet
+  const ArrayFunction = () => {
+    names = [
+        'Stig',
+        'Per',
+        'Amalie'
+    ]
+
+    
+  }
+  ArrayFunction()
+  
+  
+  // OPPGAVE 2
+  // Lag en funksjon som:
+  // 1. Sjekker lengden på en tekst
+  // 2. Hvis teksten er mer enn 20 karakterer så skal console.log returnere en feilmelding
+
+  const LengthJavaScript = () => {
+    let textLength = "Write your sentence here"
+
+  }
+  LengthJavaScript()
+
+
+// OPPGAVE 3
+  // Lag en funksjon som:
+  // 1. Tar inn en tekst
+  // 2. Gjør JAVASCRIPT om til store og små bokstaver som dette: "Javascript"
+  // 3. Sjekker om den inneholder ordet "javascript"
+  // 4. Returnerer "Fant 'javascript'!" eller "Fant ikke 'javascript'."
+  const TextJavaScript = () => {
+    let sentence = "This sentence contains the word JAVASCRIPT"
+    
+  }
+  TextJavaScript()
+
